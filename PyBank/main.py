738305b8815571d3d7113a810dcb3e02eb5fae1f
Matt Carlson <mjcarlson628@gmail.com>
2021-06-16 14:48:17 -0400

    working scripts

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,41 @@
 # Main PyBank script
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    #print(csvreader)
+
+    csv_header = next(csvreader)
+    #print(f"CSV Header: {csv_header}")
+
+    month_count = 0
+    net_total = 0
+    greatest_increase = 0
+    increase_month = ""
+    greatest_decrease = 0
+    decrease_month = ""
+    
+    for row in csvreader:
+        #print(row)
+        month_count += 1
+        net_total += int(row[1])
+
+        if int(row[1]) > greatest_increase:
+            greatest_increase = int(row[1])
+            increase_month = row[0]
+        elif int(row[1]) < greatest_decrease:
+            greatest_decrease = int(row[1])
+            decrease_month = row[0]
+    
+    print("Financial Analysis")
+    print("------------------------------------")
+    print(f"Total Months: {month_count}")
+    print(f"Total: ${net_total}")
+    print(f"Average Change: ${round(float(net_total/month_count), ndigits=2)}")
+    print(f"Greatest Increase in Profits: {increase_month} (${greatest_increase})")
+    print(f"Greatest Decrease in Profits: {decrease_month} (${greatest_decrease})")
+         
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,35 @@
 # Main PyPoll script
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+
+    total_votes = 0
+    candidates = []
+    results = {}
+
+    for row in csvreader:
+        total_votes += 1
+        #print(total_votes)
+        if row[2] not in candidates:
+            candidates.append(row[2])
+            results[row[2]] = 1
+        else:
+            results[row[2]] += 1
+
+    print("Election Results")
+    print("-------------------------------")
+    print(f"Total Votes: {total_votes}")
+    print("-------------------------------")
+    
+    for x in candidates:
+        percent_won = results[x] * 100 / total_votes
+        print(f"{x}: {format(percent_won, '.3f')}%  ({results[x]})")
+ 
+    print("-------------------------------")
+    print(f"Winner: {max(results, key = results.get)}")
+    print("-------------------------------")
