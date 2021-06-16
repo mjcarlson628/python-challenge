669a9f2860a84675cb05c2919d10a35ffcd37ff7
Matt Carlson <mjcarlson628@gmail.com>
2021-06-16 15:23:20 -0400

    commented main.py for pypoll and pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,29 +1,34 @@
 # Main PyBank script
+# Analysis of budget_data.csv
 import os
 import csv
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
 with open(csvpath) as csvfile:
+# read budget_data.csv
     csvreader = csv.reader(csvfile, delimiter=',')
-
-    #print(csvreader)
-
+# store the header row
     csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
 
+# create variables to count the number of months and total money
     month_count = 0
     net_total = 0
+
+# set the values to beat for greatest increase/decrease to 0
     greatest_increase = 0
     increase_month = ""
     greatest_decrease = 0
     decrease_month = ""
     
     for row in csvreader:
-        #print(row)
+# loop through all months
+# increase the number of months and update the net profit/loss
         month_count += 1
         net_total += int(row[1])
 
+# compare profit/loss to the current greatest value
+# store the month when there is a change in greatest increase/decrease
         if int(row[1]) > greatest_increase:
             greatest_increase = int(row[1])
             increase_month = row[0]
@@ -31,6 +36,8 @@ with open(csvpath) as csvfile:
             greatest_decrease = int(row[1])
             decrease_month = row[0]
     
+# print the number of months, net total, average monthly change,
+# and months with the greatest profit and loss
     print("Financial Analysis")
     print("------------------------------------")
     print(f"Total Months: {month_count}")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,20 +5,30 @@ import csv
 csvpath = os.path.join('Resources', 'election_data.csv')
 
 with open(csvpath) as csvfile:
+# read election_data.csv
     csvreader = csv.reader(csvfile, delimiter=',')
+# store the header row
     csv_header = next(csvreader)
 
+# running total number of votes
     total_votes = 0
+# list of candidate names
     candidates = []
+# dictionary to store each candidate's name with the votes for that person
     results = {}
 
     for row in csvreader:
+# loop through all rows and increase total vote count each time
         total_votes += 1
-        #print(total_votes)
+
+# check to see if a name has already been added to the candidates list
         if row[2] not in candidates:
+# add the name to the list if it's not already there
             candidates.append(row[2])
+# save the name to the results dictionary and count it as their first vote
             results[row[2]] = 1
         else:
+# if the name has been encountered, count it as one more vote in results
             results[row[2]] += 1
 
     print("Election Results")
@@ -27,9 +37,12 @@ with open(csvpath) as csvfile:
     print("-------------------------------")
     
     for x in candidates:
+# print the percentage and total number of votes won by each candidate
+# display the percent to 3 decimals
         percent_won = results[x] * 100 / total_votes
         print(f"{x}: {format(percent_won, '.3f')}%  ({results[x]})")
- 
+
+# find which key in the results dictionary has the highest value and declare the winner
     print("-------------------------------")
     print(f"Winner: {max(results, key = results.get)}")
     print("-------------------------------")
